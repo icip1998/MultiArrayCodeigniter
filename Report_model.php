a73d69f03020a70a2e369aa699fe7c30fa065b49
Irham Ciptadi <icip1998@gmail.com>
2019-02-11 09:50:32 +0700

    Update Report_model.php

--- Report_model.php
+++ Report_model.php
@@ -20,18 +20,6 @@ class Report_model extends CI_Model {
 
     public function get_data($id = '')
     {
-    	// $sql = "SELECT
-      //   variable_cost.*,
-      //   variable_cost_category.name as vc_category,
-      //   variable_cost_sub_category.name as vc_sub_category
-      // FROM
-      //   `variable_cost`
-      // INNER JOIN
-      //   variable_cost_category ON variable_cost_category.id = variable_cost.variable_cost_category
-      // LEFT JOIN
-      //   variable_cost_sub_category ON variable_cost_sub_category.id = variable_cost.variable_cost_sub_category
-      // ORDER BY variable_cost.vendor";
-
       $sql = "SELECT MIN(variable_cost.id) AS id, variable_cost.variable_cost_category as vc_category, variable_cost_category.name as vc_category_name
 FROM variable_cost
 INNER JOIN variable_cost_category ON variable_cost.variable_cost_category = variable_cost_category.id
@@ -58,23 +46,6 @@ GROUP BY variable_cost.variable_cost_sub_category
         $sql = "SELECT * FROM `variable_cost` WHERE variable_cost.id = $id";
         $query = $this->db->query($sql);
         return $query;
-    }
-
-    public function save($data)
-    {
-    	return $this->db->insert($this->table, $data);
-    }
-
-    public function update($id, $data)
-    {
-    	$this->db->where("id", $id);
-    	return $this->db->update($this->table, $data);
-    }
-
-    public function delete($id)
-    {
-    	$this->db->where("id", $id);
-    	$this->db->delete($this->table);
     }
 
 }
